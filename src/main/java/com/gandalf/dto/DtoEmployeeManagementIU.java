f2c496520b98c21e3d3f2bf4832477d7052c4f06
Gandalf The Grey <67333606+Gandaf-the-Grey@users.noreply.github.com>
2025-09-25 22:44:43 +0300

    updated

--- src/main/java/com/gandalf/dto/DtoEmployeeManagementIU.java
+++ src/main/java/com/gandalf/dto/DtoEmployeeManagementIU.java
@@ -17,32 +17,41 @@ import java.util.Date;
 @AllArgsConstructor
 public class DtoEmployeeManagementIU {
 
-
     @NotEmpty(message = "İsim alanı boş geçilemez.")
-    @Size(min = 3, max = 50, message = "İsim en az 3 en fazla 50 karekter içerebilir.")
+    @Size(min = 3, max = 50, message = "İsim en az 3 en fazla 50 karakter içerebilir.")
     private String firstName;
+
     @NotEmpty(message = "Soyad alanı boş geçilemez.")
-    @Size(min = 3, max = 50, message = "Soyad en az 3 en fazla 20 karekter içerebilir.")
+    @Size(min = 3, max = 50, message = "Soyad en az 3 en fazla 50 karakter içerebilir.")
     private String lastName;
+
     @NotEmpty(message = "TCKN boş olamaz.")
     @Pattern(regexp = "^[0-9]{11}$", message = "TCKN 11 haneli sayısal bir değer olmalıdır.")
     private String tckn;
-    @NotNull(message = "Dogum tarihi boş geçilemez.")
+
+    @NotNull(message = "Doğum tarihi boş geçilemez.")
     private Date birthDate;
-    @NotNull(message = "Cinsiyet alanı boş bırakılamaz.")
+
+    @NotEmpty(message = "Cinsiyet alanı boş bırakılamaz.")
     private String gender;
+
     @NotNull(message = "Çalışanın medeni hali boş geçilemez.")
     private MaritalStatus maritalStatus;
-    @NotNull
+
+    @NotNull(message = "Çalışanın mezuniyet durumu boş geçilemez.")
     private GraduationStatus graduationStatus;
+
     @NotNull(message = "Çalışan durumu boş geçilemez.")
     private Boolean isActive;
 
-    private String profilePhoto;
-    @NotNull(message = "Çalışanın departmanı alanı boş geçilemez.")
+    private String profilePhoto; // opsiyonel
+
+    @NotNull(message = "Çalışanın departmanı boş geçilemez.")
     private Department department;
-    @NotNull(message = "Çalışanın pozisyonu bpş geçilemez")
+
+    @NotNull(message = "Çalışanın pozisyonu boş geçilemez.")
     private Position position;
+
     @NotNull(message = "Çalışanın rolü boş geçilemez.")
     private Role role;
 }
